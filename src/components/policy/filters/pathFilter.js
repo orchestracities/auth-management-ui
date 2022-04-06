331a952b56b875a48bb06049616bac0af226e901
Valecant <valeriocantore@gmail.com>
2022-04-06 18:56:11 +0200

    autocomplete filter style with dummydata

--- src/components/policy/filters/pathFilter.js
+++ src/components/policy/filters/pathFilter.js
@@ -9,18 +9,22 @@ import ArchiveIcon from '@mui/icons-material/Archive';
 import FileCopyIcon from '@mui/icons-material/FileCopy';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+import Grid from '@mui/material/Grid';
+import Grow from '@mui/material/Grow';
 
 const StyledMenu = styled((props) => (
   <Menu
-    elevation={0}
     anchorOrigin={{
-      vertical: 'bottom',
-      horizontal: 'right',
+      vertical: 'top',
+      horizontal: 'left',
     }}
     transformOrigin={{
       vertical: 'top',
-      horizontal: 'right',
+      horizontal: 'left',
     }}
+
     {...props}
   />
 ))(({ theme }) => ({
@@ -28,10 +32,12 @@ const StyledMenu = styled((props) => (
     borderRadius: 6,
     marginTop: theme.spacing(1),
     minWidth: 120,
+    top: "13rem !important",
+    left: " 8rem !important",
     color:
       theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
     boxShadow:
-      'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+      'none !important',
     '& .MuiMenu-list': {
       padding: '4px 0',
     },
@@ -54,6 +60,29 @@ const StyledMenu = styled((props) => (
 export default function PathFilter() {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
+  const top100Films = [
+    { title: 'The Shawshank Redemption', year: 1994 },
+    { title: 'The Godfather', year: 1972 },
+    { title: 'The Godfather: Part II', year: 1974 },
+    { title: 'The Dark Knight', year: 2008 },
+    { title: '12 Angry Men', year: 1957 },
+    { title: "Schindler's List", year: 1993 },
+    { title: 'Pulp Fiction', year: 1994 },
+    {
+      title: 'The Lord of the Rings: The Return of the King',
+      year: 2003,
+    },
+    { title: 'The Good, the Bad and the Ugly', year: 1966 },
+    { title: 'Fight Club', year: 1999 },
+    {
+      title: 'The Lord of the Rings: The Fellowship of the Ring',
+      year: 2001,
+    },
+    {
+      title: 'Star Wars: Episode V - The Empire Strikes Back',
+      year: 1980,
+    }
+  ];
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
@@ -63,39 +92,51 @@ export default function PathFilter() {
 
   return (
     <div >
-      <Button
-      sx={{marginBottom:"30px"}}
-        id="demo-customized-button"
-        aria-controls={open ? 'demo-customized-menu' : undefined}
-        aria-haspopup="true"
-        aria-expanded={open ? 'true' : undefined}
-        variant="outlined"
-        disableElevation
-        onClick={handleClick}
-        endIcon={<KeyboardArrowDownIcon />}
+      <Grow
+        in={!open}
+        style={{ transformOrigin: '0 0 0' }}
+        {...(!open ? { timeout: 500 } : {})}
       >
-        PATH
-      </Button>
+        <Button
+          sx={{ display: (!open) ? "flex" : "none" }}
+          id="demo-customized-button"
+          aria-controls={open ? 'demo-customized-menu' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+          variant="outlined"
+          onClick={handleClick}
+          endIcon={<KeyboardArrowDownIcon />}
+        >
+          PATH
+        </Button>
+      </Grow>
       <StyledMenu
         id="demo-customized-menu"
-        MenuListProps={{
-          'aria-labelledby': 'demo-customized-button',
-        }}
+
         anchorEl={anchorEl}
         open={open}
         onClose={handleClose}
-      >
-        <MenuItem onClick={handleClose} disableRipple>
-        
-          String
-        </MenuItem>
-        <MenuItem onClick={handleClose} disableRipple>
-         
-          String
-        </MenuItem>
-      
-     
-      </StyledMenu>
+        children={
+          <Grow
+            in={open}
+            style={{ transformOrigin: '0 0 0' }}
+            {...(open ? { timeout: 500 } : {})}
+          >
+            <Autocomplete
+              id="multiple-limit-tags"
+              options={top100Films}
+              getOptionLabel={(option) => option.title}
+              renderInput={(params) => (
+                <TextField {...params} label="limitTags" placeholder="Favorites" />
+              )}
+              sx={{ width: '500px', marginTop: "10px" }}
+            />
+          </Grow>
+        }
+      />
+
+
     </div>
+
   );
 }
