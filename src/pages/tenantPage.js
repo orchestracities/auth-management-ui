f9c43f95d1d38ad34602149398ae3e33ea6752a2
Lint Action <lint-action@samuelmeuli.com>
2022-05-17 16:21:27 +0000

    Fix code style issues with Prettier

--- src/pages/tenantPage.js
+++ src/pages/tenantPage.js
@@ -1,33 +1,33 @@
-import * as React from 'react'
-import { MainTitle } from '../components/shared/mainTitle'
-import AddButton from '../components/shared/addButton'
-import { Grid } from '@mui/material'
-import SortButton from '../components/shared/sortButton'
-import DashboardCard from '../components/shared/cards'
-import TenantForm from '../components/tenant/tenantForm'
-import Grow from '@mui/material/Grow'
-import { Trans } from 'react-i18next'
+import * as React from "react";
+import { MainTitle } from "../components/shared/mainTitle";
+import AddButton from "../components/shared/addButton";
+import { Grid } from "@mui/material";
+import SortButton from "../components/shared/sortButton";
+import DashboardCard from "../components/shared/cards";
+import TenantForm from "../components/tenant/tenantForm";
+import Grow from "@mui/material/Grow";
+import { Trans } from "react-i18next";
 
-export default function TenantPage ({
+export default function TenantPage({
   tenantValues,
   getTenants,
   seTenant,
   client,
-  keycloakToken
+  keycloakToken,
 }) {
-  const [createOpen, setCreateOpen] = React.useState(false)
-  const [sortedTenants, sortTenants] = React.useState([])
-  const [count, counter] = React.useState(1)
+  const [createOpen, setCreateOpen] = React.useState(false);
+  const [sortedTenants, sortTenants] = React.useState([]);
+  const [count, counter] = React.useState(1);
   React.useEffect(() => {
     sortTenants(
       tenantValues.reverse((a, b) => parseFloat(a.name) - parseFloat(b.name))
-    )
-  }, [tenantValues])
-  const mainTitle = <Trans>tenant.titles.page</Trans>
+    );
+  }, [tenantValues]);
+  const mainTitle = <Trans>tenant.titles.page</Trans>;
   const rerOder = (newData) => {
-    sortTenants(newData)
-    counter(count + 1)
-  }
+    sortTenants(newData);
+    counter(count + 1);
+  };
 
   return (
     <div>
@@ -38,25 +38,25 @@ export default function TenantPage ({
             client={client}
             title={<Trans>tenant.titles.new</Trans>}
             close={setCreateOpen}
-            action={'create'}
+            action={"create"}
             getTenants={getTenants}
           />
         }
         setOpen={setCreateOpen}
         status={createOpen}
       ></AddButton>
-      <Grid container spacing={2} sx={{ marginLeft: '15px ' }}>
+      <Grid container spacing={2} sx={{ marginLeft: "15px " }}>
         <Grid item xs={12}>
           <SortButton
             data={sortedTenants}
-            id={'name'}
+            id={"name"}
             sortData={rerOder}
           ></SortButton>
         </Grid>
         {tenantValues.map((tenant, index) => (
           <Grow
             in={true}
-            style={{ transformOrigin: '0 0 0' }}
+            style={{ transformOrigin: "0 0 0" }}
             {...(true ? { timeout: index * 600 } : {})}
           >
             <Grid item xs={4}>
@@ -71,7 +71,7 @@ export default function TenantPage ({
                         values={{ name: tenant.name }}
                       />
                     }
-                    action={'modify'}
+                    action={"modify"}
                     tenant={tenant}
                     getTenants={getTenants}
                   ></TenantForm>
@@ -85,5 +85,5 @@ export default function TenantPage ({
         ))}
       </Grid>
     </div>
-  )
+  );
 }
