609771a23e96ec162526639f8711a22ff63ab65d
Lint Action <lint-action@samuelmeuli.com>
2022-05-19 10:40:49 +0000

    Fix code style issues with ESLint

--- graphql-server/main/mongo/tenantsQueries.js
+++ graphql-server/main/mongo/tenantsQueries.js
@@ -1,67 +1,67 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 const usrPreference = new mongoose.Schema({
   name: String,
   icon: String,
   primaryColor: String,
-  secondaryColor: String,
-});
+  secondaryColor: String
+})
 
-const Preferences = mongoose.model("UsrPreferences", usrPreference);
+const Preferences = mongoose.model('UsrPreferences', usrPreference)
 
-require("dotenv").config({ path: "../.env" });
+require('dotenv').config({ path: '../.env' })
 
-main().catch((err) => console.log(err));
+main().catch((err) => console.log(err))
 
-async function main() {
-  await mongoose.connect(process.env.GRAPHQL_MONGO_DB);
+async function main () {
+  await mongoose.connect(process.env.GRAPHQL_MONGO_DB)
 }
 
-async function get(data) {
-  const thisUser = await Preferences.find({ name: { $in: data } });
+async function get (data) {
+  const thisUser = await Preferences.find({ name: { $in: data } })
 
-  return await thisUser;
+  return await thisUser
 }
 
-async function update(data) {
-  const filter = { name: data.name };
+async function update (data) {
+  const filter = { name: data.name }
   const update = {
     name: data.name,
     icon: data.icon,
     primaryColor: data.primaryColor,
-    secondaryColor: data.secondaryColor,
-  };
+    secondaryColor: data.secondaryColor
+  }
 
-  const thisTenant = await Preferences.findOneAndUpdate(filter, update);
-  return await thisTenant;
+  const thisTenant = await Preferences.findOneAndUpdate(filter, update)
+  return await thisTenant
 }
 
-async function add(data) {
+async function add (data) {
   const arrayOfData = {
     name: data.name,
     icon: data.icon,
     primaryColor: data.primaryColor,
-    secondaryColor: data.secondaryColor,
-  };
+    secondaryColor: data.secondaryColor
+  }
 
   const thisTenant = await arrayOfData.save(function (err) {
-    if (err) return handleError(err);
-  });
-  return await thisTenant;
+    if (err) return handleError(err)
+  })
+  return await thisTenant
 }
 
-async function deleteTenant(data) {
-  const thisUser = await Preferences.find({ name: { $in: data } });
+async function deleteTenant (data) {
+  const thisUser = await Preferences.find({ name: { $in: data } })
 
   for (const e of thisUser) {
-    const deletedOwner = await Preferences.findByIdAndRemove(e._id);
+    const deletedOwner = await Preferences.findByIdAndRemove(e._id)
   }
-  return !!(await (typeof deletedOwner === "object"));
+  return !!(await (typeof deletedOwner === 'object'))
 }
 
 module.exports = {
   get,
   update,
   add,
-  deleteTenant,
-};
+  deleteTenant
+}
--- src/components/shared/userMenu.js
+++ src/components/shared/userMenu.js
@@ -1,98 +1,98 @@
-import * as React from "react";
-import Box from "@mui/material/Box";
-import Avatar from "@mui/material/Avatar";
-import Menu from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
-import Divider from "@mui/material/Divider";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import Tooltip from "@mui/material/Tooltip";
-import PersonAdd from "@mui/icons-material/PersonAdd";
-import Settings from "@mui/icons-material/Settings";
-import Logout from "@mui/icons-material/Logout";
-import AccountCircle from "@mui/icons-material/AccountCircle";
-import Dialog from "@mui/material/Dialog";
-import DialogActions from "@mui/material/DialogActions";
-import Button from "@mui/material/Button";
-import { styled } from "@mui/material/styles";
-import AppBar from "@mui/material/AppBar";
-import DialogContent from "@mui/material/DialogContent";
-import Toolbar from "@mui/material/Toolbar";
-import CloseIcon from "@mui/icons-material/Close";
-import Grid from "@mui/material/Grid";
-import InputLabel from "@mui/material/InputLabel";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
-import { useTranslation, Trans } from "react-i18next";
-import axios from "axios";
+import * as React from 'react'
+import Box from '@mui/material/Box'
+import Avatar from '@mui/material/Avatar'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import Divider from '@mui/material/Divider'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import Tooltip from '@mui/material/Tooltip'
+import PersonAdd from '@mui/icons-material/PersonAdd'
+import Settings from '@mui/icons-material/Settings'
+import Logout from '@mui/icons-material/Logout'
+import AccountCircle from '@mui/icons-material/AccountCircle'
+import Dialog from '@mui/material/Dialog'
+import DialogActions from '@mui/material/DialogActions'
+import Button from '@mui/material/Button'
+import { styled } from '@mui/material/styles'
+import AppBar from '@mui/material/AppBar'
+import DialogContent from '@mui/material/DialogContent'
+import Toolbar from '@mui/material/Toolbar'
+import CloseIcon from '@mui/icons-material/Close'
+import Grid from '@mui/material/Grid'
+import InputLabel from '@mui/material/InputLabel'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import { useTranslation, Trans } from 'react-i18next'
+import axios from 'axios'
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   useQuery,
   gql,
-  createHttpLink,
-} from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
+  createHttpLink
+} from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
 
 const DialogRounded = styled(Dialog)(({ theme }) => ({
-  "& .MuiPaper-rounded": {
-    borderRadius: 15,
-  },
-}));
+  '& .MuiPaper-rounded': {
+    borderRadius: 15
+  }
+}))
 
 const CustomDialogTitle = styled(AppBar)({
-  position: "relative",
-  background: "white",
-  boxShadow: "none",
-});
+  position: 'relative',
+  background: 'white',
+  boxShadow: 'none'
+})
 
-export default function UserMenu({ language, userData, keycloakToken }) {
-  const { t, i18n } = useTranslation();
-  const [anchorEl, setAnchorEl] = React.useState(null);
-  const [settings, setOpenSettings] = React.useState(false);
-  const open = Boolean(anchorEl);
+export default function UserMenu ({ language, userData, keycloakToken }) {
+  const { t, i18n } = useTranslation()
+  const [anchorEl, setAnchorEl] = React.useState(null)
+  const [settings, setOpenSettings] = React.useState(false)
+  const open = Boolean(anchorEl)
   const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
+    setAnchorEl(event.currentTarget)
+  }
   const handleClose = () => {
-    setAnchorEl(null);
-  };
+    setAnchorEl(null)
+  }
   const settingsOpen = () => {
-    setOpenSettings(true);
-  };
+    setOpenSettings(true)
+  }
 
   const settingsClose = () => {
-    setOpenSettings(false);
-  };
+    setOpenSettings(false)
+  }
 
   React.useEffect(() => {
-    i18n.changeLanguage(language.language);
-  }, [language]);
+    i18n.changeLanguage(language.language)
+  }, [language])
 
   const handleLanguagePreference = (newValue) => {
-    language.setLanguage(newValue);
-    newValue === "defaultBrowser"
+    language.setLanguage(newValue)
+    newValue === 'defaultBrowser'
       ? i18n.changeLanguage(Intl.NumberFormat().resolvedOptions().locale)
-      : i18n.changeLanguage(newValue);
+      : i18n.changeLanguage(newValue)
     const httpLink = createHttpLink({
-      uri: "http://localhost:4000/graphql",
-    });
+      uri: 'http://localhost:4000/graphql'
+    })
 
     const authLink = setContext((_, { headers }) => {
       return {
         headers: {
           ...headers,
-          Authorization: `Bearer ${keycloakToken}`,
-        },
-      };
-    });
+          Authorization: `Bearer ${keycloakToken}`
+        }
+      }
+    })
 
     const client = new ApolloClient({
       link: authLink.concat(httpLink),
-      cache: new InMemoryCache(),
-    });
+      cache: new InMemoryCache()
+    })
     client
       .mutate({
         mutation: gql`
@@ -108,26 +108,26 @@ export default function UserMenu({ language, userData, keycloakToken }) {
         `,
         variables: {
           usrName: userData.idTokenParsed.sub,
-          language: newValue,
-        },
+          language: newValue
+        }
       })
       .then((result) => {
-        console.log(result);
-      });
-  };
+        console.log(result)
+      })
+  }
 
   return (
     <React.Fragment>
-      <Box sx={{ display: "flex", alignItems: "center", textAlign: "center" }}>
+      <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
         <Tooltip title="Account settings">
           <IconButton
             onClick={handleClick}
             size="small"
             color="inherit"
             sx={{ ml: 2 }}
-            aria-controls={open ? "account-menu" : undefined}
+            aria-controls={open ? 'account-menu' : undefined}
             aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
+            aria-expanded={open ? 'true' : undefined}
           >
             <AccountCircle />
           </IconButton>
@@ -142,31 +142,31 @@ export default function UserMenu({ language, userData, keycloakToken }) {
         PaperProps={{
           elevation: 0,
           sx: {
-            overflow: "visible",
-            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+            overflow: 'visible',
+            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
             mt: 1.5,
-            "& .MuiAvatar-root": {
+            '& .MuiAvatar-root': {
               width: 32,
               height: 32,
               ml: -0.5,
-              mr: 1,
+              mr: 1
             },
-            "&:before": {
+            '&:before': {
               content: '""',
-              display: "block",
-              position: "absolute",
+              display: 'block',
+              position: 'absolute',
               top: 0,
               right: 10,
               width: 10,
               height: 10,
-              bgcolor: "background.paper",
-              transform: "translateY(-50%) rotate(45deg)",
-              zIndex: 0,
-            },
-          },
+              bgcolor: 'background.paper',
+              transform: 'translateY(-50%) rotate(45deg)',
+              zIndex: 0
+            }
+          }
         }}
-        transformOrigin={{ horizontal: "right", vertical: "top" }}
-        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
       >
         <MenuItem onClick={settingsOpen}>
           <ListItemIcon>
@@ -184,7 +184,7 @@ export default function UserMenu({ language, userData, keycloakToken }) {
       <DialogRounded
         open={settings}
         fullWidth={true}
-        maxWidth={"md"}
+        maxWidth={'md'}
         onClose={settingsClose}
         aria-labelledby="alert-dialog-titlel"
         aria-describedby="alert-dialog-descriptionl"
@@ -195,7 +195,7 @@ export default function UserMenu({ language, userData, keycloakToken }) {
               <CloseIcon />
             </IconButton>
             <Typography
-              sx={{ ml: 2, flex: 1, color: "black" }}
+              sx={{ ml: 2, flex: 1, color: 'black' }}
               variant="h6"
               component="div"
             >
@@ -208,7 +208,7 @@ export default function UserMenu({ language, userData, keycloakToken }) {
             <Grid item xs={12}>
               <FormControl fullWidth>
                 <InputLabel id="language">
-                  {" "}
+                  {' '}
                   <Trans>common.userSettings.language</Trans>
                 </InputLabel>
                 <Select
@@ -216,14 +216,14 @@ export default function UserMenu({ language, userData, keycloakToken }) {
                   id="language-select"
                   variant="outlined"
                   onChange={(event) => {
-                    handleLanguagePreference(event.target.value);
+                    handleLanguagePreference(event.target.value)
                   }}
                   value={language.language}
                   label={<Trans>common.userSettings.language</Trans>}
                 >
-                  <MenuItem value={"defaultBrowser"}>Default</MenuItem>
-                  <MenuItem value={"en"}>English</MenuItem>
-                  <MenuItem value={"it"}>Italian</MenuItem>
+                  <MenuItem value={'defaultBrowser'}>Default</MenuItem>
+                  <MenuItem value={'en'}>English</MenuItem>
+                  <MenuItem value={'it'}>Italian</MenuItem>
                 </Select>
               </FormControl>
             </Grid>
@@ -232,5 +232,5 @@ export default function UserMenu({ language, userData, keycloakToken }) {
         <DialogActions></DialogActions>
       </DialogRounded>
     </React.Fragment>
-  );
+  )
 }
