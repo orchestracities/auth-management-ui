183fb1beba491f539463bea30a8f33fe499c4e3e
Lint Action <lint-action@samuelmeuli.com>
2022-05-20 08:16:17 +0000

    Fix code style issues with ESLint

--- graphql-server/main/mongo/populateDB.js
+++ graphql-server/main/mongo/populateDB.js
@@ -1,57 +1,57 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-require("dotenv").config({ path: "../.env" });
+require('dotenv').config({ path: '../.env' })
 
-const connection = mongoose.createConnection(process.env.GRAPHQL_MONGO_DB);
+const connection = mongoose.createConnection(process.env.GRAPHQL_MONGO_DB)
 
 const usrPreference = new mongoose.Schema({
   name: String,
   icon: String,
   primaryColor: String,
-  secondaryColor: String,
-});
+  secondaryColor: String
+})
 
-const Preferences = connection.model("UsrPreferences", usrPreference);
+const Preferences = connection.model('UsrPreferences', usrPreference)
 
 const usrSettings = new mongoose.Schema({
   usrName: String,
-  language: String,
-});
+  language: String
+})
 
-const Settings = connection.model("UsrSettings", usrSettings);
+const Settings = connection.model('UsrSettings', usrSettings)
 
 Preferences.deleteMany({}, function (err, small) {
-  console.log('clear everything that was here before..');
+  console.log('clear everything that was here before..')
   Preferences.create(
     {
-      name: "Tenant1",
-      icon: "none",
-      primaryColor: "#8086ba",
-      secondaryColor: "#8086ba",
+      name: 'Tenant1',
+      icon: 'none',
+      primaryColor: '#8086ba',
+      secondaryColor: '#8086ba'
     },
     function (err, small) {
       if (err) {
-        return handleError(err);
+        return handleError(err)
       } else {
-        console.log("Tenant1 created!");
+        console.log('Tenant1 created!')
         Preferences.create(
           {
-            name: "Tenant2",
-            icon: "none",
-            primaryColor: "#8086ba",
-            secondaryColor: "#8086ba",
+            name: 'Tenant2',
+            icon: 'none',
+            primaryColor: '#8086ba',
+            secondaryColor: '#8086ba'
           },
           function (err, small) {
             if (err) {
-              return handleError(err);
+              return handleError(err)
             } else {
-              console.log("Tenant2 created!");
-              process.exit();
+              console.log('Tenant2 created!')
+              process.exit()
             }
           }
-        );
+        )
       }
     }
   )
 }
-);
+)
--- src/components/shared/cards.js
+++ src/components/shared/cards.js
@@ -1,70 +1,70 @@
-import * as React from "react";
-import { styled } from "@mui/material/styles";
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import Avatar from "@mui/material/Avatar";
-import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
-import MultifunctionButton from "./speedDial";
-import Badge from "@mui/material/Badge";
-import SecurityIcon from "@mui/icons-material/Security";
-import ContentCopyIcon from "@mui/icons-material/ContentCopy";
-import ServiceChildren from "../service/serviceChildren";
-import PoliciesChildren from "../policy/policiesChildren";
-import IconList from "../tenant/iconList";
-import { Trans } from "react-i18next";
+import * as React from 'react'
+import { styled } from '@mui/material/styles'
+import Card from '@mui/material/Card'
+import CardHeader from '@mui/material/CardHeader'
+import CardMedia from '@mui/material/CardMedia'
+import CardContent from '@mui/material/CardContent'
+import CardActions from '@mui/material/CardActions'
+import Avatar from '@mui/material/Avatar'
+import IconButton from '@mui/material/IconButton'
+import Typography from '@mui/material/Typography'
+import MultifunctionButton from './speedDial'
+import Badge from '@mui/material/Badge'
+import SecurityIcon from '@mui/icons-material/Security'
+import ContentCopyIcon from '@mui/icons-material/ContentCopy'
+import ServiceChildren from '../service/serviceChildren'
+import PoliciesChildren from '../policy/policiesChildren'
+import IconList from '../tenant/iconList'
+import { Trans } from 'react-i18next'
 
 const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
+  const { expand, ...other } = props
+  return <IconButton {...other} />
 })(({ theme, expand }) => ({
-  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
-  marginLeft: "auto",
-  transition: theme.transitions.create("transform", {
-    duration: theme.transitions.duration.shortest,
-  }),
-}));
-const RadiusDiv = styled("div")(({ theme }) => ({
-  borderRadius: "15px",
+  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest
+  })
+}))
+const RadiusDiv = styled('div')(({ theme }) => ({
+  borderRadius: '15px',
   background: theme.palette.primary.light
-    .replace(")", " / 70% )")
-    .replace(/,/g, ""),
+    .replace(')', ' / 70% )')
+    .replace(/,/g, ''),
   color: theme.palette.primary.contrastText,
-  maxWidth: 550,
-}));
+  maxWidth: 550
+}))
 
-export default function DashboardCard({ pageType, data, getData, seTenant }) {
-  const [subpathOpen, setSubpathOpen] = React.useState(false);
-  const [status, setOpen] = React.useState(false);
-  const props = { close: setOpen };
-  const listOfIcons = IconList();
+export default function DashboardCard ({ pageType, data, getData, seTenant }) {
+  const [subpathOpen, setSubpathOpen] = React.useState(false)
+  const [status, setOpen] = React.useState(false)
+  const props = { close: setOpen }
+  const listOfIcons = IconList()
   const iconMapper = (iconName) => {
-    const thisIcon = listOfIcons.filter((e) => e.name === iconName);
-    return thisIcon[0].name !== "none" ? thisIcon[0].icon : data.name[0];
-  };
-  const layout = React.cloneElement(pageType, props);
+    const thisIcon = listOfIcons.filter((e) => e.name === iconName)
+    return thisIcon[0].name !== 'none' ? thisIcon[0].icon : data.name[0]
+  }
+  const layout = React.cloneElement(pageType, props)
   const incrementColor = (color, step) => {
-    let colorToInt = parseInt(color.substr(1), 16);
-    const nstep = parseInt(step);
+    let colorToInt = parseInt(color.substr(1), 16)
+    const nstep = parseInt(step)
     if (!isNaN(colorToInt) && !isNaN(nstep)) {
-      colorToInt += nstep;
-      let ncolor = colorToInt.toString(16);
-      ncolor = "#" + new Array(7 - ncolor.length).join(0) + ncolor;
+      colorToInt += nstep
+      let ncolor = colorToInt.toString(16)
+      ncolor = '#' + new Array(7 - ncolor.length).join(0) + ncolor
       if (/^#[0-9a-f]{6}$/i.test(ncolor)) {
-        return ncolor;
+        return ncolor
       }
     }
-    return color;
-  };
+    return color
+  }
   return (
     <RadiusDiv
       key={data.id}
       sx={{
         background:
-          layout.props.action === "Sub-service-creation" ? "" : "#8086bab8",
+          layout.props.action === 'Sub-service-creation' ? '' : '#8086bab8'
       }}
     >
       <CardHeader
@@ -72,16 +72,16 @@ export default function DashboardCard({ pageType, data, getData, seTenant }) {
           <Avatar
             sx={{
               bgcolor:
-                layout.props.action === "Sub-service-creation"
+                layout.props.action === 'Sub-service-creation'
                   ? incrementColor(
-                      layout.props.tenantName_id[0].props.primaryColor,
-                      Math.floor(Math.random() * 500) - 20
-                    )
-                  : data.props.primaryColor,
+                    layout.props.tenantName_id[0].props.primaryColor,
+                    Math.floor(Math.random() * 500) - 20
+                  )
+                  : data.props.primaryColor
             }}
             aria-label="recipe"
           >
-            {layout.props.action === "Sub-service-creation"
+            {layout.props.action === 'Sub-service-creation'
               ? data.path[1]
               : iconMapper(data.props.icon)}
           </Avatar>
@@ -97,15 +97,15 @@ export default function DashboardCard({ pageType, data, getData, seTenant }) {
           ></MultifunctionButton>
         }
         title={
-          layout.props.action === "Sub-service-creation" ? data.path : data.name
+          layout.props.action === 'Sub-service-creation' ? data.path : data.name
         }
         subheader={<Typography variant="body2">{data.id}</Typography>}
       />
       <CardContent>
         <Typography variant="body2">
-          {layout.props.action === "Sub-service-creation"
+          {layout.props.action === 'Sub-service-creation'
             ? layout.props.tenantName_id[0].name
-            : "description"}
+            : 'description'}
         </Typography>
       </CardContent>
       <CardActions>
@@ -113,23 +113,25 @@ export default function DashboardCard({ pageType, data, getData, seTenant }) {
           setOpen={setSubpathOpen}
           status={subpathOpen}
           data={
-            layout.props.action === "modify"
+            layout.props.action === 'modify'
               ? data.service_paths.slice(1)
               : data.children
           }
-          masterTitle={layout.props.action === "modify" ? data.name : data.path}
+          masterTitle={layout.props.action === 'modify' ? data.name : data.path}
           getData={getData}
         />
-        {layout.props.action === "Sub-service-creation" ? (
-          ""
-        ) : (
+        {layout.props.action === 'Sub-service-creation'
+          ? (
+              ''
+            )
+          : (
           <PoliciesChildren
             tenantId={data.id}
             tenantName={data.name}
             seTenant={seTenant}
           ></PoliciesChildren>
-        )}
+            )}
       </CardActions>
     </RadiusDiv>
-  );
+  )
 }
