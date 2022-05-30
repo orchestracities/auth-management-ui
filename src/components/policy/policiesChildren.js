22ebb1dc8f2d433ddfa77b56b1cd5f902574a76a
Lint Action <lint-action@samuelmeuli.com>
2022-05-30 08:14:00 +0000

    Fix code style issues with ESLint

--- src/components/policy/policiesChildren.js
+++ src/components/policy/policiesChildren.js
@@ -1,69 +1,69 @@
-import * as React from "react";
-import IconButton from "@mui/material/IconButton";
-import { styled } from "@mui/material/styles";
-import Badge from "@mui/material/Badge";
-import PolicyIcon from "@mui/icons-material/Policy";
-import axios from "axios";
-import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import * as React from 'react'
+import IconButton from '@mui/material/IconButton'
+import { styled } from '@mui/material/styles'
+import Badge from '@mui/material/Badge'
+import PolicyIcon from '@mui/icons-material/Policy'
+import axios from 'axios'
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom'
 
-export default function PoliciesChildren({ tenantId, tenantName, seTenant }) {
+export default function PoliciesChildren ({ tenantId, tenantName, seTenant }) {
   // services
-  const [services, setServices] = React.useState([{ children: [] }]);
+  const [services, setServices] = React.useState([{ children: [] }])
   const getServices = () => {
     axios
       .get(
         process.env.REACT_APP_ANUBIS_API_URL +
-          "v1/tenants/" +
+          'v1/tenants/' +
           tenantId +
-          "/service_paths"
+          '/service_paths'
       )
       .then((response) => {
-        getPolicies(response.data);
+        getPolicies(response.data)
       })
       .catch((e) => {
-        console.error(e);
-      });
-  };
+        console.error(e)
+      })
+  }
   // policies
-  const [policies, setPolicies] = React.useState([{ children: [] }]);
+  const [policies, setPolicies] = React.useState([{ children: [] }])
   const getPolicies = (servicesResponse) => {
-    let datAccumulator = [];
+    let datAccumulator = []
     for (const service of servicesResponse) {
       axios
-        .get(process.env.REACT_APP_ANUBIS_API_URL + "v1/policies", {
+        .get(process.env.REACT_APP_ANUBIS_API_URL + 'v1/policies', {
           headers: {
-            "fiware-service": tenantName,
-            "fiware-servicepath": service.path,
-          },
+            'fiware-service': tenantName,
+            'fiware-servicepath': service.path
+          }
         })
         .then((response) => {
-          response.data.forEach((e) => (e.fiware_service = tenantId));
-          response.data.forEach((e) => (e.fiware_service_path = service.path));
-          datAccumulator = [...datAccumulator, ...response.data];
-          setPolicies(datAccumulator);
+          response.data.forEach((e) => (e.fiware_service = tenantId))
+          response.data.forEach((e) => (e.fiware_service_path = service.path))
+          datAccumulator = [...datAccumulator, ...response.data]
+          setPolicies(datAccumulator)
         })
         .catch((e) => {
-          console.error(e);
-        });
+          console.error(e)
+        })
     }
-    console.log(policies);
-  };
+    console.log(policies)
+  }
 
   const reiderect = () => {
-    seTenant(tenantId);
-  };
+    seTenant(tenantId)
+  }
 
   React.useEffect(() => {
-    getServices();
-  }, [tenantId]);
+    getServices()
+  }, [tenantId])
 
   return (
-    <NavLink to={"/Policy"}>
+    <NavLink to={'/Policy'}>
       <IconButton aria-label="service" onClick={reiderect}>
         <Badge badgeContent={policies.length} color="secondary">
           <PolicyIcon color="primary" fontSize="large" />
         </Badge>
       </IconButton>
     </NavLink>
-  );
+  )
 }
