b6cec471d0692fc6c9a5e3676cc32ccb964ea330
Valecant <valeriocantore@gmail.com>
2022-06-09 11:00:04 +0200

    oidc sessions implemented

--- graphql-server/main/advancedAuth.js
+++ graphql-server/main/advancedAuth.js
@@ -7,6 +7,8 @@ const {  buildContext } = require("graphql-passport");
 const app = express()
 const { get, update, add, deleteTenant } = require('./mongo/tenantsQueries')
 const { getUserPref, updateUserPref } = require('./mongo/usrSettings')
+const { Issuer, Strategy } = require ('openid-client');
+const expressSession = require ('express-session');
 
 const typeDefs = gql`
   type TenantConfiguration {
@@ -69,19 +71,42 @@ const resolvers = {
   }
 }
 
-
+async function startServer() {
 app.use(cors())
 
-app.get('/auth/keycloak',
-  passport.authenticate('keycloak', { scope: ['profile'] }));
+const thisIssuer = await Issuer.discover('http://localhost:8080/auth/realms/master')
 
-app.get('/auth/keycloak/callback', 
-  passport.authenticate('keycloak', { failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
+
+const client = new thisIssuer.Client({
+    client_id: 'master',
+    redirect_uris: ['http://localhost:8080/auth/realms/master/protocol/openid-connect/auth?client_id=client1&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2F&state=2646d2b3-7215-45ec-9e0a-ebd09fd69998&response_mode=fragment&response_type=code&scope=openid&nonce=e46becd8-84eb-4af8-8852-f643df11b0ff'],
+    post_logout_redirect_uris: ['http://localhost:3000'],
+    response_types: ['code'],
   });
+  var memoryStore = new expressSession.MemoryStore();
+  app.use(
+      expressSession({
+        secret: 'another_long_secret',
+      resave: false,
+      saveUninitialized: true,
+      store: memoryStore
+      })
+  );
 
-async function startServer() {
+  app.use(passport.initialize());
+  app.use(passport.authenticate('session'));
+  passport.use('oidc', new Strategy({client}, (tokenSet, userinfo, done)=>{
+          return done(null, tokenSet.claims());
+      })
+  )
+  
+  passport.serializeUser(function(user, done) {
+      done(null, user);
+    });
+  passport.deserializeUser(function(user, done) {
+      done(null, user);
+  });
+  
   const apolloServer = new ApolloServer({
     typeDefs: [typeDefs],
     resolvers,
