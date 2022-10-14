c373bda2b623a4d8f4eae2b0926eeebe3c025f1f
Valecant <valeriocantore@gmail.com>
2022-10-14 23:25:44 +0200

    back to storybook stable release

--- documentation/.storybook/main.js
+++ documentation/.storybook/main.js
@@ -1,8 +1,5 @@
 module.exports = {
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
-  framework: {
-    name: '@storybook/react-webpack5',
-    options: {}
-  }
+  framework: '@storybook/react'
 };
