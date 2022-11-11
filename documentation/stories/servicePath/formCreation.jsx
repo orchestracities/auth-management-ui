e5c6a4bc2902d5230dea65121f3a4737f31f90c2
Valecant <valeriocantore@gmail.com>
2022-11-11 16:53:05 +0100

    documentation

---
+++ documentation/stories/servicePath/formCreation.jsx
@@ -0,0 +1,89 @@
+import PropTypes from 'prop-types';
+import Grid from '@mui/material/Grid';
+import ResourceForm from '../../../src/components/resource/resourceForm';
+import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { orange } from '@mui/material/colors';
+import '../../../src/i18n';
+import React from 'react';
+import { SnackbarProvider } from 'notistack';
+
+/**
+ * Primary UI component for user interaction
+ */
+const theme = createTheme({
+  status: {
+    danger: orange[500]
+  }
+});
+
+export const ResourceTypeCreation = ({ title, close, action, token, tokenData, env, getTheResources, thisTenant }) => {
+  return (
+    <SnackbarProvider maxSnack={5}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Grid>
+            <ResourceForm
+              title={title}
+              close={close}
+              action={action}
+              token={token}
+              tokenData={tokenData}
+              env={env}
+              getTheResources={getTheResources}
+              thisTenant={thisTenant}
+            />
+          </Grid>
+        </BrowserRouter>
+      </ThemeProvider>
+    </SnackbarProvider>
+  );
+};
+
+ResourceTypeCreation.propTypes = {
+  /**
+   * The title that will be displayed inside the form
+   */
+  title: PropTypes.string,
+  /**
+   * The hook function to close the modal
+   */
+   close: PropTypes.func,
+
+  /**
+   * The token passed by keycloack
+   */
+   token: PropTypes.string,
+    /**
+   * The form action
+   */
+  action: PropTypes.oneOf(['create']),
+ /**
+   * The data inside the token after the decode
+   */
+  tokendata: PropTypes.object,
+ /**
+   * The ENV variables
+   */
+  env: PropTypes.object,
+   /**
+   * The function to call to get the resources
+   */
+    getTheResources: PropTypes.func,
+  /**
+   * The Tenant name
+   */
+   thisTenant: PropTypes.string,
+  
+};
+
+ResourceTypeCreation.defaultProps = {
+  title:"",
+  close:undefined,
+  action:"create",
+  token:"",
+  tokendata:{},
+  env:{},
+  getTheResources:undefined,
+  thisTenant:""
+};
---
+++ documentation/stories/servicePath/table.jsx
@@ -0,0 +1,70 @@
+import PropTypes from 'prop-types';
+import Grid from '@mui/material/Grid';
+import ResourceTable from '../../../src/components/resource/resourceTypeTable';
+import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { orange } from '@mui/material/colors';
+import '../../../src/i18n';
+import React from 'react';
+import { SnackbarProvider } from 'notistack';
+
+/**
+ * Primary UI component for user interaction
+ */
+const theme = createTheme({
+  status: {
+    danger: orange[500]
+  }
+});
+
+export const ServicePathTable = ({ token, tokenData, env, resources, getTheResources }) => {
+  return (
+    <SnackbarProvider maxSnack={5}>
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Grid>
+            <ResourceTable
+              token={token}
+              tokenData={tokenData}
+              env={env}
+              resources={resources}
+              getTheResources={getTheResources}
+            />
+          </Grid>
+        </BrowserRouter>
+      </ThemeProvider>
+    </SnackbarProvider>
+  );
+};
+
+ServicePathTable.propTypes = {
+  /**
+   * The user token passed by Keycloack
+   */
+  token: PropTypes.string,
+  /**
+   * The user token decoded passed by Keycloack
+   */
+   tokenData: PropTypes.object,
+
+  /**
+   * the ENV variables
+   */
+   env: PropTypes.object,
+ /**
+   * the array of values that will be displayed
+   */
+  resources: PropTypes.arrayOf(PropTypes.object),
+  /**
+   * The function to get the resource types data after a save
+   */
+   getTheResources: PropTypes.func,
+};
+
+ServicePathTable.defaultProps = {
+    token:"",
+    tokenData:{},
+    env:{},
+    resources:[],
+    getTheResources:undefined
+};
