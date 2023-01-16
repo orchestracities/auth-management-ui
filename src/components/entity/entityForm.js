bed64f25b87a0442cd3ecc1b4ac2a51549594454
Valecant <valeriocantore@gmail.com>
2023-01-16 20:32:34 +0100

    entity modify freeze

--- src/components/entity/entityForm.js
+++ src/components/entity/entityForm.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
 import { styled } from '@mui/material/styles';
 import DialogContent from '@mui/material/DialogContent';
 import AppBar from '@mui/material/AppBar';
@@ -22,12 +23,57 @@ import { Trans } from 'react-i18next';
 import * as log from 'loglevel';
 import isURL from 'validator/lib/isURL';
 import axios from 'axios';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import InputAdornment from '@mui/material/InputAdornment';
+import 'dayjs/locale/en';
+import 'dayjs/locale/it';
+import ClearIcon from '@mui/icons-material/Clear';
+import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+import Tooltip from '@mui/material/Tooltip';
+import AddIcon from '@mui/icons-material/Add';
 
 const CustomDialogTitle = styled(AppBar)({
   position: 'relative',
   background: 'white',
   boxShadow: 'none'
 });
+
+const CustomSwitch = styled(Switch)(({ theme }) => ({
+  padding: 8,
+  '& .MuiSwitch-track': {
+    borderRadius: 22 / 2,
+    '&:before, &:after': {
+      content: '""',
+      position: 'absolute',
+      top: '50%',
+      transform: 'translateY(-50%)',
+      width: 16,
+      height: 16,
+    },
+    '&:before': {
+      backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 24 24"><path fill="${encodeURIComponent(
+        theme.palette.getContrastText(theme.palette.primary.main),
+      )}" d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z"/></svg>')`,
+      left: 12,
+    },
+    '&:after': {
+      backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 24 24"><path fill="${encodeURIComponent(
+        theme.palette.getContrastText(theme.palette.primary.main),
+      )}" d="M19,13H5V11H19V13Z" /></svg>')`,
+      right: 12,
+    },
+  },
+  '& .MuiSwitch-thumb': {
+    boxShadow: 'none',
+    width: 16,
+    height: 16,
+    margin: 2,
+  },
+}));
+
 
 export default function EntityForm({
   title,
@@ -76,6 +122,7 @@ export default function EntityForm({
   const handleClose = () => {
     close(false);
   };
+  const rowTypes = ["Number", "DateTime", "Boolean", "Text"]
   const [service, setService] = React.useState('');
   const [type, setType] = React.useState('');
   const [name, setName] = React.useState('');
@@ -101,12 +148,123 @@ export default function EntityForm({
   React.useEffect(() => {
     createAttributesMap();
   }, []);
-
+const addEntities=()=>{
+  setAttributesMap([...[{ name: '', type: '', value: '' }], ...attributesMap]);
+}
+const removeEntities=(index)=>{
+  const newArray = attributesMap;
+    newArray.splice(index, 1);
+    setAttributesMap([...[], ...newArray]);
+}
   const handleAttributeName = (value, index) => {
     const newArray = attributesMap;
     newArray[Number(index)].name = value;
     setAttributesMap([...[], ...newArray]);
   };
+  const handleAttributeType = (value, index) => {
+    const newArray = attributesMap;
+    newArray[Number(index)].type = value;
+    newArray[Number(index)].value = '';
+    setAttributesMap([...[], ...newArray]);
+  };
+  const handlePropagation = (e) => {
+    e.stopPropagation();
+  };
+  const attributeTypeForm = (attribute, index) => {
+    switch (attribute.type !== "" && typeof attribute.type !== "undefined" && attribute.type !== null) {
+      case attribute.type === 'Number':
+        return (<Grid item xs={12}>
+          <TextField
+            id={'Number' + index}
+            label="Number"
+            variant="outlined"
+            key={'Number' + index}
+            value={attribute.value}
+            type="number"
+            onChange={(event) => {
+              const newArray = attributesMap;
+              newArray[Number(index)].value = event.target.value;
+              setAttributesMap([...[], ...newArray]);
+            }}
+            sx={{
+              width: '100%'
+            }}
+          />
+        </Grid>);
+      case attribute.type === 'DateTime':
+        return (<Grid item xs={12}>
+          <Grid container direction="row" justifyContent="center" alignItems="center" spacing={0}>
+            <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={Intl.NumberFormat().resolvedOptions().locale}>
+
+              <MobileDatePicker
+                id={'dateInput' + index}
+                key={'dateInput' + index}
+                showToolbar={false}
+                value={(typeof attribute.value !== "object") ? new Date() : attribute.value}
+                onChange={(newValue) => {
+                  const newArray = attributesMap;
+                  newArray[Number(index)].value = Number(newValue);
+                  setAttributesMap([...[], ...newArray]);
+                }}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="start" onClick={handlePropagation}>
+                          <ClearIcon
+                            onClick={() => {
+                              const newArray = attributesMap;
+                              newArray[Number(index)].value = null;
+                              setAttributesMap([...[], ...newArray]);
+                            }}
+                            sx={{ '&:hover': { cursor: 'pointer' } }}
+                          />
+                        </InputAdornment>
+                      )
+                    }}
+                  />
+                )}
+                sx={{ width: '100%' }}
+              />
+            </LocalizationProvider>
+          </Grid>
+        </Grid>);
+      case attribute.type === 'Boolean':
+        return (<Grid item xs={12}>
+          <Grid container direction="row" justifyContent="center" alignItems="center" spacing={0}>
+            <FormControlLabel
+              key={"switch" + index}
+              id={"switch" + index}
+              control={<CustomSwitch defaultChecked onChange={(event) => {
+                const newArray = attributesMap;
+                newArray[Number(index)].value = event.target.checked;
+                setAttributesMap([...[], ...newArray]);
+              }} />}
+            />
+          </Grid>
+        </Grid>);
+      case attribute.type === 'Text':
+        return (<Grid item xs={12}>
+          <TextField
+            id="Text"
+            label="Text"
+            variant="outlined"
+            value={attribute.value}
+            onChange={(event) => {
+              const newArray = attributesMap;
+              newArray[Number(index)].value = event.target.value;
+              setAttributesMap([...[], ...newArray]);
+            }}
+            sx={{
+              width: '100%'
+            }}
+          />
+        </Grid>);
+      default:
+        return <></>;
+    }
+  }
   const handleType = (event) => {
     setType(event.target.value);
   };
@@ -144,14 +302,14 @@ export default function EntityForm({
       const headers =
         service !== ''
           ? {
-              'fiware-Service': GeTenantData('name'),
-              //'Authorization': `Bearer ${token}`,
-              'fiware-ServicePath': service[service.length - 1] === '/' ? service + '#' : service + '/#'
-            }
+            'fiware-Service': GeTenantData('name'),
+            //'Authorization': `Bearer ${token}`,
+            'fiware-ServicePath': service[service.length - 1] === '/' ? service + '#' : service + '/#'
+          }
           : {
-              'fiware-Service': GeTenantData('name')
-              //'Authorization': `Bearer ${token}`,
-            };
+            'fiware-Service': GeTenantData('name')
+            //'Authorization': `Bearer ${token}`,
+          };
       switch (action) {
         case 'create':
           axios
@@ -303,22 +461,93 @@ export default function EntityForm({
           ) : (
             <>
               {attributesMap.map((attribute, i) => (
-                <Grid item xs={12}>
-                  <TextField
-                    id={'name' + i}
-                    label="Name"
-                    variant="outlined"
-                    key={'name' + i}
-                    value={attribute.name}
-                    onChange={(event) => {
-                      handleAttributeName(event.target.value, i);
-                    }}
-                    sx={{
-                      width: '100%'
-                    }}
-                  />
-                </Grid>
+                  <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
+                  <Grid container spacing={12} direction="row" justifyContent="center" alignItems="center">
+                    <Grid item xs={10} sm={10} md={10} lg={10} xl={10}>
+                      <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                          <TextField
+                            id={'name' + i}
+                            label="Name"
+                            variant="outlined"
+                            key={'name' + i}
+                            value={attribute.name}
+                            onChange={(event) => {
+                              handleAttributeName(event.target.value, i);
+                            }}
+                            sx={{
+                              width: '100%'
+                            }}
+                          />
+                        </Grid>
+                        <Grid item xs={12}>
+                          <FormControl fullWidth>
+                            <InputLabel id={'rowType' + i} error={errorCases(type.type)}>
+                              <Trans>entity.form.type</Trans>
+                            </InputLabel>
+                            <Select
+                              color="secondary"
+                              labelId={'rowType' + i}
+                              id={'rowType' + i}
+                              key={'rowType' + i}
+                              value={attribute.type}
+                              variant="outlined"
+                              onChange={(event) => {
+                                handleAttributeType(event.target.value, i);
+                              }}
+                              label={<Trans>entity.form.type</Trans>}
+                              input={<OutlinedInput label="Types" />}
+                              error={errorCases(type.type)}
+                            >
+                              {rowTypes.map((thisType) => (
+                                <MenuItem key={thisType} value={thisType}>
+                                  {thisType}
+                                </MenuItem>
+                              ))}
+                            </Select>
+                            <FormHelperText error={errorCases(type.type)}>{errorText(type.type)}</FormHelperText>
+                          </FormControl>
+                        </Grid>
+                        {attributeTypeForm(attribute, i)}
+                      </Grid>
+                    </Grid>
+                    <Grid item xs={1} sm={1} md={1} lg={1} xl={1}>
+                      <Grid
+                        container
+                        direction="row"
+                        justifyContent="center"
+                        alignItems="center"
+                      >
+                        <Tooltip title={<Trans>common.deleteTooltip</Trans>}>
+                          <IconButton
+                            aria-label="delete"
+                            size="large"
+                            onClick={() => {
+                              removeEntities(i);
+                            }}
+                          >
+                            <DeleteIcon fontSize="inherit" />
+                          </IconButton>
+                        </Tooltip>
+                      </Grid>
+                    </Grid>
+                  </Grid>
+                   </Grid>
               ))}
+              <Grid item xs={12}>
+                  {' '}
+                  <Grid container direction="row" justifyContent="center" alignItems="center" spacing={0}>
+                    <Button
+                      variant="outlined"
+                      startIcon={<AddIcon />}
+                      onClick={() => {
+                        addEntities();
+                      }}
+                    >
+                      New Entity
+                    </Button>
+                  </Grid>
+                </Grid>
             </>
           )}
         </Grid>
