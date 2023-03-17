2da99b69b9b6117d6f028f9c932ab62dde6224c5
Valecant <valeriocantore@gmail.com>
2023-03-17 11:27:07 +0100

    up

--- public/OidcTrustedDomains.js
+++ public/OidcTrustedDomains.js
@@ -4,12 +4,4 @@
 
 // Domains used by OIDC server must be also declared here
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-const trustedDomains = {
-  default: ['https://demo.duendesoftware.com', 'https://kdhttps.auth0.com'],
-  config_classic: ['https://demo.duendesoftware.com'],
-  config_without_silent_login: ['https://demo.duendesoftware.com'],
-  config_without_refresh_token: ['https://demo.duendesoftware.com'],
-  config_without_refresh_token_silent_login: ['https://demo.duendesoftware.com'],
-  config_google: ['https://oauth2.googleapis.com', 'https://openidconnect.googleapis.com'],
-  config_with_hash: ['https://demo.duendesoftware.com']
-};
+const trustedDomains = {};
