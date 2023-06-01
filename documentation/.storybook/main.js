c391fa53cec10a8af838b38464b17f01b0140fe5
Valecant <valeriocantore@gmail.com>
2023-06-01 16:16:18 +0200

    update storybook and API doc

--- documentation/.storybook/main.js
+++ documentation/.storybook/main.js
@@ -1,11 +1,6 @@
 module.exports = {
   stories: ['../stories/**/*.stories.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-docs'
-  ],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-docs', '@storybook/addon-mdx-gfm'],
   framework: {
     name: '@storybook/react-webpack5',
     options: {}
